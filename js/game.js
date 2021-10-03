d666a85df94f1ba1b06c0fcccde5dd656d0331f5
Eli Granat <91833720+EliGranat@users.noreply.github.com>
2021-10-03 17:52:03 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -24,7 +24,7 @@ var gGame = {
 };
 window.onload=init;
 function init() {
-    document.addEventListener('keydown', changeMoveImgPac);
+    document.addEventListener('keyup', changeMoveImgPac);
     gBoard = buildBoard();
     createPacman(gBoard);
     createGhosts(gBoard);
